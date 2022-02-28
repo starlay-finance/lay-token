60ed09243f5a0554b8cf30111f0216688036c9eb
atatur9 <momodarou2015@gmail.com>
2022-02-28 12:47:46 +0900

    add vesting owner

--- tasks/deployments/transfer-from-vesting.ts
+++ tasks/deployments/transfer-from-vesting.ts
@@ -1,3 +1,4 @@
+import { Signer, Wallet } from 'ethers';
 import { parseEther } from 'ethers/lib/utils';
 import { task } from 'hardhat/config';
 import {
@@ -12,6 +13,10 @@ const { LayToken, LayTokenImpl } = eContractid;
 
 task(`transfer-from-vesting`, `transfer amount from vesting`).setAction(
   async ({ verify }, localBRE) => {
+    const privateKey = process.env.VESTING_OWNER_PRIVATE_KEY || '';
+    if (!privateKey) {
+      throw new Error('INVALID_VESTING_OWNER_PRIVATE_KEY');
+    }
     await localBRE.run('set-dre');
 
     if (!localBRE.network.config.chainId) {
@@ -20,8 +25,12 @@ task(`transfer-from-vesting`, `transfer amount from vesting`).setAction(
 
     console.log(`\n- Transfer LAY from vesting contract`);
     const vestingInstance = await getTokenVesting('0xebb855938652225212a7Dbd50Bd78Ba3e22d7B23');
+    const wallet = new Wallet(privateKey);
     console.log(`vesting address ${vestingInstance.address}`);
-    const tx = await vestingInstance.withdraw(parseEther('250000000'));
+    console.log(`owner address: ${wallet.address}`);
+    const tx = await vestingInstance
+      .connect(new Wallet(privateKey))
+      .withdraw(parseEther('250000000'));
     tx.wait(1);
     console.log('finished transfer 250000000 $LAY');
     console.log(tx);
