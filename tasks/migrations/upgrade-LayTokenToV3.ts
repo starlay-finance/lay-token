e30631bdad77514c4c3a901766ea8932d4c3640d
atatur9 <momodarou2015@gmail.com>
2022-05-27 13:01:57 +0900

    upgrade lay to V3 deploy script

--- tasks/migrations/upgrade-LayTokenToV3.ts
+++ tasks/migrations/upgrade-LayTokenToV3.ts
@@ -1,29 +1,24 @@
 import { InitializableAdminUpgradeabilityProxy } from '../../types/InitializableAdminUpgradeabilityProxy';
 import { task } from 'hardhat/config';
 import { eContractid } from '../../helpers/types';
-import {
-  getLayToken,
-  getLayTokenImpl,
-  getContract,
-  getTokenVesting,
-  getStarlayRewardsVault,
-  getStarlayRewardsVaultProxy,
-  getLayTokenV3,
-} from '../../helpers/contracts-helpers';
-import { waitForTx } from '../../helpers/misc-utils';
-import { ZERO_ADDRESS } from '../../helpers/constants';
+import { getLayToken, getContract, getLayTokenV3 } from '../../helpers/contracts-helpers';
+import { Wallet } from 'ethers';
+import { JsonRpcProvider } from '@ethersproject/providers';
 
-const { LayToken } = eContractid;
+const { LayToken, LayTokenV3 } = eContractid;
 
-task(`upgrade-${LayToken}`, `Initialize the ${LayToken} proxy contract`)
-  .addParam('admin', `The address to be added as an Admin role in ${LayToken} Transparent Proxy.`)
-  .addFlag('onlyProxy', 'Initialize only the proxy contract, not the implementation contract')
-  .setAction(async ({ admin: layAdmin, onlyProxy }, localBRE) => {
+task(`upgrade-${LayTokenV3}`, `Initialize the ${LayTokenV3} proxy contract`).setAction(
+  async ({}, localBRE) => {
     await localBRE.run('set-dre');
+    const LAY_ADMIN_PRIVATE_KEY = process.env.LAY_ADMIN_PRIVATE_KEY || '';
+    const admin = new Wallet(
+      LAY_ADMIN_PRIVATE_KEY,
+      new JsonRpcProvider('https://astar.public.blastapi.io')
+    );
 
-    if (!layAdmin) {
+    if (!LAY_ADMIN_PRIVATE_KEY) {
       throw new Error(
-        `Missing --admin parameter to add the Admin Role to ${LayToken} Transparent Proxy`
+        `Missing layAdmin Private key parameter to upgrade ${LayToken} Transparent Proxy`
       );
     }
 
@@ -31,16 +26,17 @@ task(`upgrade-${LayToken}`, `Initialize the ${LayToken} proxy contract`)
       throw new Error('INVALID_CHAIN_ID');
     }
 
-    console.log(`\n- ${LayToken} initialization`);
+    console.log(`\n- ${LayTokenV3} initialization`);
 
-    const layTokenImpl = await getLayTokenV3();
-    const layToken = await getLayToken();
+    const layTokenV3 = await getLayTokenV3('0x2d10e23201861AFCa95Af0126d79C5a4D1bd277d');
+    const layToken = await getLayToken('0xc4335B1b76fA6d52877b3046ECA68F6E708a27dd');
     const layTokenProxy = await getContract<InitializableAdminUpgradeabilityProxy>(
       eContractid.InitializableAdminUpgradeabilityProxy,
       layToken.address
     );
-    const encodedInitialize = await layTokenImpl.interface.encodeFunctionData('initialize');
-    layTokenProxy.upgradeToAndCall(layTokenImpl.address, encodedInitialize);
+    const encodedInitialize = await layTokenV3.interface.encodeFunctionData('initialize');
+    layTokenProxy.connect(admin).upgradeToAndCall(layTokenV3.address, encodedInitialize);
 
-    console.log('\tFinished Lay Token and Transparent Proxy initialization');
-  });
+    console.log('\tFinished Lay Token upgrade to V3');
+  }
+);
