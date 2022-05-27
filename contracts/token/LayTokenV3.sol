a28959cfadb2d79c4f5ecc7e2f09cc11d6ad290f
atatur9 <momodarou2015@gmail.com>
2022-05-27 12:47:40 +0900

    refactor

--- contracts/token/LayTokenV3.sol
+++ contracts/token/LayTokenV3.sol
@@ -17,7 +17,7 @@ contract LayTokenV3 is GovernancePowerDelegationERC20, VersionedInitializable {
   string internal constant NAME = 'Lay Token';
   string internal constant SYMBOL = 'LAY';
   uint8 internal constant DECIMALS = 18;
-  address internal immutable VAULT = 0x43822E69852Ca59211bBC1Bd429feAb6A62A9f44;
+  address internal constant VAULT = 0x43822E69852Ca59211bBC1Bd429feAb6A62A9f44;
 
   uint256 public constant REVISION = 3;
 
